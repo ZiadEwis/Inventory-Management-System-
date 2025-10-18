4a06d64f154d5b075034e28b410f032c9a5b2c22
eyadmotawe <eyadmotawe@gmail.com>
2025-10-18 04:40:29 +0300

    added Product Constructor

--- src/DataBaseItem/Product.java
+++ src/DataBaseItem/Product.java
@@ -10,6 +10,16 @@ public class Product extends DatabaseItem{
     private String supplierName;
     private float price;
     private int quantity;
+    public Product(String productID, String productName, String manufacturerName, String
+            supplierName, int quantity, float price)
+    {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
     public int getQuantity()
     {
         return quantity;
